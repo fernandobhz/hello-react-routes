f87be1be0b0c6c6fd69c3c848ad6bb48b8b47047
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-10-26 20:04:24 -0300

    showing how react router works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { HashRouter, Route, useParams, Link } from "react-router-dom";
+
+function Soma({ a, b }) {
+  return (
+    <div>
+      <h2>Soma</h2>
+      <p>{Number(a) + Number(b)}</p>
+    </div>
+  );
+}
+
+function Multiplicacao({ a, b }) {
+  return (
+    <div>
+      <h2>Multiplicação</h2>
+      <p>{Number(a) * Number(b)}</p>
+    </div>
+  );
+}
+
+function UsaSoma() {
+  const { a, b } = useParams();
+  return <Soma a={a} b={b} />;
+}
+
+function UsaMultiplicacao() {
+  const { a, b } = useParams();
+  return <Multiplicacao a={a} b={b} />;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HashRouter>
+        <h1>Menu principal</h1>
+        <Link to="/soma/10/200">Soma</Link><br />
+        <Link to="/multiplicacao/10/200">Multiplicação</Link>
+
+        <hr />
+        <h1>Usando HashRouter</h1>
+
+        <Route path="/soma/:a/:b" component={UsaSoma} />
+        <Route path="/multiplicacao/:a/:b" component={UsaMultiplicacao} />
+      </HashRouter>
+      <hr />
+
+      <h1>Usando Renderizacao Condicional</h1>
+      {window.location.hash.startsWith("#/soma") && (
+        <>
+          <Soma
+            a={window.location.hash.split("/").reverse()[0]}
+            b={window.location.hash.split("/").reverse()[1]}
+          />
+        </>
+      )}
+      {window.location.hash.startsWith("#/multiplicacao") && (
+        <>
+          <Multiplicacao
+            a={window.location.hash.split("/").reverse()[0]}
+            b={window.location.hash.split("/").reverse()[1]}
+          />
+        </>
+      )}
+    </>
   );
 }
 
